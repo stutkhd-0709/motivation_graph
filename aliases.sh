908fc7d001d43c75516468df3539669103c004b9
Takahide Sato <whydoyoustudy@gmail.com>
2021-02-20 18:56:41 +0900

    [F] タイポ

--- aliases.sh
+++ aliases.sh
@@ -1,4 +1,4 @@
 alias dev='docker-compose up dev'
 alias dev-b='docker-compose up --build dev'
-alias deb-bash='docker exec -it dev bash'
+alias dev-bash='docker exec -it motivation_graph bash'
 alias get-tw='docker-compose run --rm get_tweet'
